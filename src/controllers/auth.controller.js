ec138e40739ca6b1d259d5ddde741eab378cd81f
diegooo555 <132108395+diegooo555@users.noreply.github.com>
2024-10-09 09:31:55 -0500

    Max Duration Cookie Setting

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,7 +33,8 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'None'
+            sameSite: 'None',
+            maxAge: 24 * 60 * 60 * 1000
         });
         res.json({
             id: userSaved._id,
@@ -66,7 +67,8 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'None'
+            sameSite: 'None',
+            maxAge: 24 * 60 * 60 * 1000
         });
         res.json({
             id: userFound._id,
